5a545909fb159083ec31d7131858a84d9498861f
abel <raorolli@gmail.com>
2021-09-18 08:59:38 +0800

    add prescription and contract wasm file

--- advaita_health_contract/lib.rs
+++ advaita_health_contract/lib.rs
@@ -13,6 +13,7 @@ mod advaita_health_contract {
     };
     use ink_prelude::vec::Vec;
     use ink_prelude::string::String;
+    use ink_prelude::string::ToString;
     use ink_lang::static_assertions::_core::borrow::Borrow;
     use ink_storage::traits::{PackedLayout, SpreadLayout, KeyPtr};
     use ink_primitives::Key;
@@ -20,11 +21,14 @@ mod advaita_health_contract {
 
     #[ink(storage)]
     pub struct AdvaitaHealthContract {
-        // todo need to multi contract
+        // todo need to multi contract,update table? consider dApp
         questionnaires: StorageMap<(AccountId, u64), Survey>,
         indexes: StorageMap<AccountId, u64>,
         tradition_questionnaires: StorageMap<(AccountId, u64), Survey>,
         tr_indexes: StorageMap<AccountId, u64>,
+        prescription: StorageMap<(AccountId, u64), Prescription>,
+        pr_indexes: StorageMap<AccountId, u64>,
+
     }
 
 
@@ -59,11 +63,15 @@ mod advaita_health_contract {
             let indexes: StorageMap<AccountId, u64> = StorageMap::new();
             let tr_questionnaires: StorageMap<(AccountId, u64), Survey> = StorageMap::new();
             let tr_indexes: StorageMap<AccountId, u64> = StorageMap::new();
+            let prescription: StorageMap<(AccountId, u64), Prescription> = StorageMap::new();
+            let pr_indexes: StorageMap<AccountId, u64> = StorageMap::new();
             Self {
                 questionnaires: quest_map,
                 indexes,
                 tradition_questionnaires: tr_questionnaires,
                 tr_indexes,
+                prescription,
+                pr_indexes,
             }
         }
 
@@ -137,6 +145,42 @@ mod advaita_health_contract {
             }
             vec
         }
+
+        #[ink(message)]
+        pub fn add_prescription(&mut self, info: Prescription) {
+            let caller = Self::env().caller();
+            let index = self.pr_indexes.get(&caller).unwrap_or(&0);
+            let new_index = index + 1;
+            self.prescription.insert((caller.clone(), *index), info);
+            self.pr_indexes.insert(caller, new_index);
+            Self::env().emit_event(Update {
+                owner: caller,
+            });
+        }
+
+
+        #[ink(message)]
+        pub fn get_prescription(&self, index: u64) -> Prescription {
+            let caller = Self::env().caller();
+            let re = self.prescription.get(&(caller, index)).cloned().unwrap_or(Prescription::default());
+            Self::env().emit_event(Query {
+                owner: caller,
+            });
+            return re;
+        }
+
+
+        #[ink(message)]
+        pub fn prescription_list(&self) -> Vec<Prescription> {
+            let caller = Self::env().caller();
+            let count = self.pr_indexes.get(&caller).unwrap_or(&0);
+            let mut vec: Vec<Prescription> = Vec::new();
+            for index in 0..(*count) {
+                let survey = self.prescription.get(&(caller, index)).cloned().unwrap_or(Prescription::default());
+                vec.push(survey);
+            }
+            vec
+        }
     }
 
     #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
@@ -164,6 +208,56 @@ mod advaita_health_contract {
         fn clone(&self) -> Self {
             Self {
                 content: self.content.clone()
+            }
+        }
+    }
+
+    #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(
+    feature = "std",
+    derive(
+    Debug,
+    PartialEq,
+    Eq,
+    scale_info::TypeInfo,
+    ink_storage::traits::StorageLayout
+    )
+    )]
+    pub struct Prescription {
+        pub doctor_name: String,
+        pub doctor_id: String,
+        pub doctor_phone: String,
+        pub patient_name: String,
+        pub patient_phone: String,
+        pub patient_id: String,
+        pub content: String,
+    }
+
+
+    impl Default for Prescription {
+        fn default() -> Self {
+            Prescription {
+                doctor_name: "".to_string(),
+                doctor_id: "".to_string(),
+                doctor_phone: "".to_string(),
+                patient_name: "".to_string(),
+                patient_phone: "".to_string(),
+                patient_id: "".to_string(),
+                content: "".to_string(),
+            }
+        }
+    }
+
+    impl Clone for Prescription {
+        fn clone(&self) -> Self {
+            Self {
+                doctor_name: self.doctor_name.clone(),
+                doctor_id: self.doctor_id.clone(),
+                doctor_phone: self.doctor_phone.clone(),
+                patient_name: self.patient_name.clone(),
+                patient_phone: self.patient_phone.clone(),
+                patient_id: self.patient_id.clone(),
+                content: self.content.clone(),
             }
         }
     }
